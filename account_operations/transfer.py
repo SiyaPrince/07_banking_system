c54664284eb6bb5a7770b25c5fd8e6908bb75f19
SiyaPrince <nkosiprince02@gmail.com>
2026-08-25 12:23:10 +0200

    transfer run plan

---
+++ account_operations/transfer.py
@@ -0,0 +1,12 @@
+# Validate sender
+# Validate receiver
+# Validate amount
+# Check sufficient funds
+
+# Only after all checks pass:
+
+# Debit sender
+# Credit receiver
+# Create sender transaction
+# Create receiver transaction
+# Display success
