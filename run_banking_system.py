21e475318abc97e88dd3ad38db9f901b1aedf02f
SiyaPrince <nkosiprince02@gmail.com>
2026-08-25 13:13:34 +0200

    initial implementations

--- run_banking_system.py
+++ run_banking_system.py
@@ -1,11 +1,39 @@
-# Register customer
-# View customers
-# Create account
-# View accounts
-# Deposit money
-# Withdraw money
-# Transfer money between accounts
-# View transaction history
-# View account balance
-# Display banking summary
-# Exit
+from support_operations.displayers import display_welcome_message, display_menu
+    
+def run_bank_system():
+    customers = []
+    accounts = []
+    transactions = []
+
+    display_welcome_message()
+
+    while True:
+        display_menu()
+        operation_choice = input("\nPlease choose operation: ").strip()
+
+        if operation_choice == "1":
+            register_customer(customers)
+        elif operation_choice == "2":
+            view_customers(customers)
+        elif operation_choice == "3":
+            create_account(accounts)
+        elif operation_choice == "4":
+            view_accounts(accounts)
+        elif operation_choice == "5":
+            deposit_money(accounts, transactions)
+        elif operation_choice == "6":
+            withdraw_money(accounts, transactions)
+        elif operation_choice == "7":
+            transfer_money(accounts, transactions)
+        elif operation_choice == "8":
+            view_transaction_history(transactions)
+        elif operation_choice == "9":
+            view_account_balance(accounts)
+        elif operation_choice == "10":
+            display_banking_summary(customers, accounts, transactions)
+        elif operation_choice == "11":
+            print("\nExiting Prince Siya Bank. Goodbye!")
+            break
+        else:
+            print("\nInvalid choice. Please try again.")
+    
---
+++ support_operations/displayers.py
@@ -0,0 +1,20 @@
+def display_welcome_message():
+    print("=" * 45)
+    print("\nWelcome to Prince Siya Bank")
+    print("=" * 45)
+
+def display_menu():
+    print("=" * 45)
+    print()
+    print("1. Register Customer")
+    print("2. View Customers")
+    print("3. Create Account")
+    print("4. View Accounts")
+    print("5. Deposit Money")
+    print("6. Withdraw Money")
+    print("7. Transfer Money")
+    print("8. View Transacion History")
+    print("9. View Account Balance")
+    print("10. Display Banking Summary")
+    print("11. Exit")
+    print("=" * 45)
