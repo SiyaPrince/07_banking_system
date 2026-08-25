6309839014e690d5c8422efd5fd273529555ac82
SiyaPrince <nkosiprince02@gmail.com>
2026-08-25 12:20:34 +0200

    operations to run bank system

---
+++ run_banking_system.py
@@ -0,0 +1,11 @@
+# Register customer
+# View customers
+# Create account
+# View accounts
+# Deposit money
+# Withdraw money
+# Transfer money between accounts
+# View transaction history
+# View account balance
+# Display banking summary
+# Exit
